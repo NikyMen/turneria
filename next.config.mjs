ea1ff2b9b50ba894a18b05c621462481b2fed518
Nicolas Mendez <niko.moto@hotmail.com>
2026-03-28 02:52:09 -0300

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: false,
+  webpack: (config, { dev, isServer }) => {
+    // Desactivamos el cache de Webpack en desarrollo para evitar el error de Buffer
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
